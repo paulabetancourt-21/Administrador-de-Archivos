1fad42b8cd9c78f2c979451c3b16e7d6eb31b5e6
Paula Betancourt <paula.betancourt@uptc.edu.co>
2026-02-16 11:11:17 -0500

    Prueba con scanner

--- src/co/edu/uptc/presenter/Presenter.java
+++ src/co/edu/uptc/presenter/Presenter.java
@@ -1,11 +1,20 @@
 package co.edu.uptc.presenter;
+
+import co.edu.uptc.view.IOManager;
+
 /*Author: Paula Andrea Betancourt Matamoros
  * Date: 
  * Description: 
  */
 public class Presenter {
+    private IOManager io; 
 
+    public String saludar(){
+        String saludo = "Hola mundo"; 
+        return saludo; 
+    }
 
     public void init() {
+        io.showMessage(saludar());
     }
 }
--- src/co/edu/uptc/view/IOManager.java
+++ src/co/edu/uptc/view/IOManager.java
@@ -3,15 +3,22 @@ package co.edu.uptc.view;
  * Date: 
  * Description: 
  */
-import javax.swing.JOptionPane;
+import java.util.Scanner; 
 
 public class IOManager {
+    private Scanner console; 
+
+    public IOManager(){
+        console = new Scanner(System.in); 
+    }
+
     public void showMessage(String message){
-        JOptionPane.showMessageDialog(null, message);
+        System.out.println(message);
     }
 
     public String readData(String message){
-        String data = JOptionPane.showInputDialog(message);
+        System.out.println(message);
+        String data = console.nextLine(); 
         return data;
     }
 }
